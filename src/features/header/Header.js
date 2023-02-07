d8db437a48ef77d6a9a4727f66614b3b14871da1
Peter van der Meer <plvdmeer@gmail.com>
2023-02-07 15:55:43 +0100

    added Header

---
+++ src/features/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeaderImg from './Header.png'
+
+
+const Header = () => {
+
+  return (
+    <header className="header pure-g">
+      <img src={HeaderImg} className="pure-u-1" />
+    </header>
+  )
+}
+
+export default Header
