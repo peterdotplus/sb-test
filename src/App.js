3a51f5b003d9e293fb3dc760981e9ba36590c5e4
Peter van der Meer <plvdmeer@gmail.com>
2023-02-08 15:32:07 +0100

    added Pure's Responsive Grids CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './grids-responsive-min.css';
 
 import Header from './features/header/Header'
 
@@ -7,6 +8,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div class="pure-g">
+        <div class="pure-u-1 pure-u-md-1-2"> ... </div>
+        <div class="pure-u-1 pure-u-md-1-2"> ... </div>
+    </div>
     </div>
   );
 }
